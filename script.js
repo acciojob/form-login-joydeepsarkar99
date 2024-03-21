47b85da6d0e84f13daa65b770c1cccd3f106cd81
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-21 13:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
+	//element selector
+	// let nameInput = document.querySelectorAll("input")
 
+	//attribute selctor
+	let fnameInput = document.querySelector('[name="fname"]')
+	let lnameInput = document.querySelector('[name="lname"]')
+	let userName = fnameInput.value+" "+lnameInput.value
+	alert(userName)
 }
